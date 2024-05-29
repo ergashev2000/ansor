91d163b5f169eb29f3b28ea18d974d0b38160763
Islomjon <ergashevislomjon1020@gmail.com>
2024-05-29 16:30:38 +0500

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,12 @@ import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
+  const siteUrl = "https://www.ansorsafety.uz/";
+
   return {
-    metadataBase: new URL("https://www.ansorsafety.uz/"),
-    title: "Ansor safety",
+    metadataBase: new URL(siteUrl),
+    title: "Ansor Safety",
     description:
       "Ansor Safety MCHJ - avtotransport va mashinasozlik mahsulotlarini sinovlari va sertifikatlashtirish ishlarini bajaradi.",
     keywords: [
@@ -18,6 +20,36 @@ export async function generateMetadata() {
       "Avtotransport va mashinasozlik mahsulotlarini sinovlari",
       "Sertifikatlashtirish",
     ],
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      url: siteUrl,
+      title: "Ansor Safety",
+      description:
+        "Ansor Safety MCHJ - avtotransport va mashinasozlik mahsulotlarini sinovlari va sertifikatlashtirish ishlarini bajaradi.",
+      siteName: "Ansor Safety",
+      images: [
+        {
+          url: `${siteUrl}images/logo.png`,
+          width: 800,
+          height: 600,
+          alt: "Ansor Safety Logo",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@AnsorSafety",
+      title: "Ansor Safety",
+      description:
+        "Ansor Safety MCHJ - avtotransport va mashinasozlik mahsulotlarini sinovlari va sertifikatlashtirish ishlarini bajaradi.",
+      images: [`${siteUrl}images/logo.png`],
+    },
+    robots: "index, follow",
+    viewport: "width=device-width, initial-scale=1.0",
+    alternates: {
+      canonical: siteUrl,
+    },
   };
 }
 
